da819bda53fa1f7ee1ab6b0645a03bf36a1e9e62
Sanzz16 <112499152+Sanzz16@users.noreply.github.com>
2026-03-04 05:21:29 +0700

    Add files via upload

--- pages/api/user/profile.ts
+++ pages/api/user/profile.ts
@@ -60,7 +60,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       upd.password_hash = await hashPassword(password)
     }
 
-    if (Object.keys(upd).length === 1)
+    if (Object.keys(upd).length <= 1)
       return res.status(400).json({ error: 'Tidak ada perubahan' })
 
     const { data, error } = await supabaseAdmin.from('users')
